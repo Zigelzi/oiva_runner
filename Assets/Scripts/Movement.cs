4adf8c36edde55345c9e751bf7b9091b34abdf9a
Miika Savela <miika.a.savela@gmail.com>
2023-11-08 17:59:48 +0200

    Add basic movement towards right

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelScroller : MonoBehaviour
+public class Movement : MonoBehaviour
 {
+    [SerializeField] private float _movementSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * _movementSpeed * Time.deltaTime);
     }
 }
