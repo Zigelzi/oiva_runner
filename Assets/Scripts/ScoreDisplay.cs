efece08b20cb2cf9d001b9677848a73dcda6cd12
Miika Savela <miika.a.savela@gmail.com>
2023-11-13 08:14:42 +0200

    Add updating travel distance in UI

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -6,20 +6,39 @@ public class ScoreDisplay : MonoBehaviour
     [SerializeField] private TMP_Text _travelDistance;
     [SerializeField] private TMP_Text _scooterThrowDistance;
 
+    private Movement _movement;
+
     private void Awake()
     {
+        _movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+
         InitialiseScores();
+    }
+
+    private void OnEnable()
+    {
+        _movement.onTravelDistanceChanged.AddListener(UpdateTravelDistance);
+    }
+
+    private void OnDisable()
+    {
+        _movement.onTravelDistanceChanged.RemoveListener(UpdateTravelDistance);
     }
 
     private void InitialiseScores()
     {
         if (_travelDistance)
         {
-            _travelDistance.text = "0";
+            _travelDistance.text = "0 m";
         }
         if (_scooterThrowDistance)
         {
             _scooterThrowDistance.text = "0";
         }
+    }
+
+    private void UpdateTravelDistance(int newDistance)
+    {
+        _travelDistance.text = newDistance.ToString() + " m";
     }
 }
