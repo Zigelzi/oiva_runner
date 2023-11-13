f6534ce6fb4cba8f54beec64ca344744caa4c8cb
Miika Savela <miika.a.savela@gmail.com>
2023-11-13 08:08:12 +0200

    Add broadcasting the distance player has traveled

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    private Vector3 _startingPosition;
+
+    public TravelDistanceChanged onTravelDistanceChanged;
+
+    [Serializable]
+    public class TravelDistanceChanged : UnityEvent<int> { }
+
+    private void Awake()
+    {
+        _startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * _movementSpeed * Time.deltaTime);
+        GetTravelDistance();
+    }
+
+    private void GetTravelDistance()
+    {
+        int distance = (int)Vector3.Distance(_startingPosition, transform.position);
+
+        onTravelDistanceChanged?.Invoke(distance);
     }
 }
