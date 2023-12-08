a9c2b703dbeb610ea076056cbe83dd7cba3720fb
Miika Savela <miika.a.savela@gmail.com>
2023-12-08 14:40:04 +0200

    Add ability for player to throw the scooter by stopping the tapping

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -7,12 +7,16 @@ public class Throwing : MonoBehaviour
     [SerializeField] private Transform _carryingPosition;
     [SerializeField] private float _maxThrowForce = 20f;
     [SerializeField] private float _timeToThrow = 2f;
+    [SerializeField] private float _throwThresholdTime = .5f;
     [SerializeField] private float _throwForceIncrement = 2f;
 
     private Scooter _currentScooter;
     private int _currentThrowDistance = 0;
     private float _currentThrowForce = 0f;
+    private Coroutine _currentThrowTimer;
+    [SerializeField] private float _currentTimeToThrow = -1f;
     private Goal _goal;
+    private int _interactionCount = 0;
     private Status _playerStatus;
 
     public Transform CarryingPosition { get { return _carryingPosition; } }
@@ -27,12 +31,29 @@ public class Throwing : MonoBehaviour
     {
         _goal = FindAnyObjectByType<Goal>();
         _playerStatus = GetComponent<Status>();
+
+        _currentTimeToThrow = _throwThresholdTime;
     }
     private void OnEnable()
     {
         _goal.onGoalReach.AddListener(Disable);
         _playerStatus.onObstacleHit.AddListener(Disable);
         Scooter.onScooterDestroy += UpdateThrowDistance;
+    }
+
+    private void Update()
+    {
+        if (_interactionCount > 0)
+        {
+            _currentTimeToThrow -= Time.deltaTime;
+        }
+        if (_currentTimeToThrow <= 0)
+        {
+            Throw();
+
+            if (_currentThrowTimer == null) return;
+            StopCoroutine(_currentThrowTimer);
+        }
     }
 
     private void OnDisable()
@@ -52,7 +73,7 @@ public class Throwing : MonoBehaviour
 
             _currentScooter = newScooter;
             newScooter.Follow(_carryingPosition);
-            StartCoroutine(StartThrowTimer());
+            _currentThrowTimer = StartCoroutine(StartThrowTimer());
         }
     }
 
@@ -61,6 +82,11 @@ public class Throwing : MonoBehaviour
         if (!_currentScooter) return;
         if (!enabled) return;
 
+        if (_interactionCount > 0)
+        {
+            _currentTimeToThrow = _throwThresholdTime;
+        }
+        _interactionCount += 1;
         _currentThrowForce += _throwForceIncrement;
         _currentThrowForce = Mathf.Min(_currentThrowForce, _maxThrowForce);
     }
@@ -75,6 +101,8 @@ public class Throwing : MonoBehaviour
         _currentScooter.Throw(throwDirection, _currentThrowForce);
         _currentScooter = null;
         _currentThrowForce = 0f;
+        _interactionCount = 0;
+        _currentTimeToThrow = _throwThresholdTime;
         onScooterThrow?.Invoke();
     }
 
