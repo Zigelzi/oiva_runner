945fc3a1c6c64e1828d7a6a96f25752e5872b5ca
Miika Savela <miika.a.savela@gmail.com>
2023-12-17 18:29:08 +0200

    Add spawning scooters with levels

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -5,12 +5,13 @@ public class LevelSpawner : MonoBehaviour
 {
     [SerializeField] private List<Transform> _levelPrefabs;
     [SerializeField] private int _numberOfLevels = 3;
+    [SerializeField] private Scooter _scooterPrefab;
     [SerializeField] private Vector3 _spawnOffset = new Vector3(25, 0, 5);
 
     private void Awake()
     {
         if (_levelPrefabs.Count <= 0) return;
-        SpawnLevels(_numberOfLevels);
+        SpawnLevel(_numberOfLevels);
     }
 
     private void OnEnable()
@@ -25,10 +26,10 @@ public class LevelSpawner : MonoBehaviour
 
     private void SpawnAdditionalLevels()
     {
-        SpawnLevels(1, 100);
+        SpawnLevel(1, 100);
     }
 
-    private void SpawnLevels(int spawnCount, int additionalXOffset = 0)
+    private void SpawnLevel(int spawnCount, int additionalXOffset = 0)
     {
         for (int i = 0; i < spawnCount; i++)
         {
@@ -37,7 +38,18 @@ public class LevelSpawner : MonoBehaviour
                 transform.position.x + (_spawnOffset.x * i) + additionalXOffset,
                 transform.position.y + _spawnOffset.y,
                 _spawnOffset.z);
-            Instantiate(_levelPrefabs[spawnIndex], spawnPosition, Quaternion.identity, transform);
+            Transform instantiatedLevel = Instantiate(_levelPrefabs[spawnIndex], spawnPosition, Quaternion.identity, transform);
+            SpawnScooter(instantiatedLevel);
         }
+    }
+
+    private void SpawnScooter(Transform parent)
+    {
+        if (!_scooterPrefab) return;
+
+        Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(60f, 120f), 0);
+        Vector3 spawnPosition = new Vector3(parent.position.x, parent.position.y, parent.position.z - 5f);
+        Instantiate(_scooterPrefab, spawnPosition, spawnRotation, parent);
+
     }
 }
